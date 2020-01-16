414f57b89ed7a2a453a626e8f6bccc11d67abb1e
Artsiom.Shylau <a.shilov.by@gmail.com>
2020-01-16 16:52:28 +0300

    husky fix

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -8,22 +8,22 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger.json';
 
 export class App {
-  public app: express.Application;
+	public app: express.Application;
 
-  constructor() {
-    this.app = express();
-    this.setDatabases();
-    this.setAppConfig();
-  }
+	constructor() {
+		this.app = express();
+		this.setDatabases();
+		this.setAppConfig();
+	}
 
-  private setAppConfig(): void {
-    this.app.use(bodyParser.json());
-    this.app.use(bodyParser.urlencoded({ extended: true }));
-    // this.app.use('/api/v1', IndexRoute);
-    this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-  }
+	private setAppConfig(): void {
+		this.app.use(bodyParser.json());
+		this.app.use(bodyParser.urlencoded({ extended: true }));
+		// this.app.use('/api/v1', IndexRoute);
+		this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+	}
 
-  private setDatabases(): void {
-    mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
-  }
+	private setDatabases(): void {
+		mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+	}
 }
